1003e20258347e7b0a073633a2314cac22648da0
ivoryguru <ivoryguru@pm.me>
2020-12-17 11:19:00 -0600

    Deleting IRC to be restored in a future version possibly.

--- app/src/main/java/com/aeon/app/irc/Message.java
+++
@@ -1,9 +0,0 @@
-package com.aeon.app.irc;
-
-class Message {
-    public String origin;
-    public String nickname;
-    public String command;
-    public String target;
-    public String content;
-}
